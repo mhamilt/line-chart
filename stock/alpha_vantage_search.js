62cb846cf5e49a154c87d19c7c40f9cc30f837f9
Matthew Hamilton <matthew.hamilton2@unibo.it>
2025-12-15 14:52:51 +0000

    test av

---
+++ stock/alpha_vantage_search.js
@@ -0,0 +1,72 @@
+let symbol = "MSFT"
+let result = new Array(5);
+
+function alphaVantageQuery(symbol)
+{
+    let wibble = "5M5AQ6D0IR1C36LG";
+    let financialsUrl = `https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=${symbol}&apikey=${wibble}`
+    let ttmUrl = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=${wibble}`
+    
+    
+    // JSON.parse(jsonString) 
+    // result[0] = ttmResult.RevenueTTM
+    // result[1:5] = // result.annualReports[0:4].costOfRevenue
+    
+    const financialsRequest = new XMLHttpRequest();
+    financialsRequest.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let response = JSON.parse(financialsRequest.responseText)
+            
+            console.log(response);
+            for (let i = 1; i < 6; i++) {
+                result[i] = response.annualReports[i].costOfRevenue;
+                document.getElementById(`m${i}`).innerText = result[i];
+            }
+            
+            result  = result.reverse();
+            let chartQuery = `?${result[0]}`
+            for (let i = 1; i < 6; i++) {                
+                chartQuery +=`&${result[i]}`                
+            }
+
+            console.log(result);
+            document.getElementById(`revenue-chart`).src = `./chartjs/${chartQuery}`
+            
+        }
+    };
+    
+    financialsRequest.open("GET", financialsUrl, true);
+    financialsRequest.send();
+    
+    const ttmRequest = new XMLHttpRequest();
+    
+    ttmRequest.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let response = JSON.parse(ttmRequest.responseText)
+            console.log(response);
+            // console.log(response.RevenueTTM)
+            result[0] = response.RevenueTTM;
+            document.getElementById(`ttm`).innerText = response.RevenueTTM;            
+            document.getElementById("company-name").innerText = response.Name;
+            document.getElementById("company-description").innerText =  response.Description;
+            document.getElementById("exchange").innerText = response.Exchange;
+            document.getElementById("sector").innerText =   response.Sector;
+        }
+    };
+    
+    ttmRequest.open("GET", ttmUrl, true);
+    ttmRequest.send();
+    
+    return result;
+}
+
+// AJAX query
+// return result
+
+addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+      symbol = document.getElementById(`searchbar`).value.toUpperCase().replaceAll(" ",'')
+      console.log(symbol)
+      alphaVantageQuery(symbol)
+    }
+});
