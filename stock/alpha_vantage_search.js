3a83c5ee4bfcff55134f618799ce9d43eefa470a
Matthew Hamilton <matthew.hamilton2@unibo.it>
2025-12-15 22:04:09 +0000

    scale axes

--- stock/alpha_vantage_search.js
+++ stock/alpha_vantage_search.js
@@ -3,14 +3,12 @@ let result = new Array(5);
 
 function alphaVantageQuery(symbol)
 {
+    cleardata();
+
     let wibble = "5M5AQ6D0IR1C36LG";
     let financialsUrl = `https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=${symbol}&apikey=${wibble}`
     let ttmUrl = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=${wibble}`
     
-    
-    // JSON.parse(jsonString) 
-    // result[0] = ttmResult.RevenueTTM
-    // result[1:5] = // result.annualReports[0:4].costOfRevenue
     
     const financialsRequest = new XMLHttpRequest();
     financialsRequest.onreadystatechange = function() {
@@ -19,16 +17,16 @@ function alphaVantageQuery(symbol)
             
             console.log(response);
             for (let i = 1; i < 6; i++) {
-                result[i] = response.annualReports[i].costOfRevenue;
+                result[i] = response.annualReports[i].costOfRevenue/1000000;
                 document.getElementById(`m${i}`).innerText = result[i];
             }
             
             result  = result.reverse();
             let chartQuery = `?${result[0]}`
-            for (let i = 1; i < 6; i++) {                
-                chartQuery +=`&${result[i]}`                
+            for (let i = 1; i < 6; i++) {
+                chartQuery +=`&${result[i]}`
             }
-
+            
             console.log(result);
             document.getElementById(`revenue-chart`).src = `./chartjs/${chartQuery}`
             
@@ -36,7 +34,7 @@ function alphaVantageQuery(symbol)
     };
     
     financialsRequest.open("GET", financialsUrl, true);
-    financialsRequest.send();
+    
     
     const ttmRequest = new XMLHttpRequest();
     
@@ -45,8 +43,8 @@ function alphaVantageQuery(symbol)
             let response = JSON.parse(ttmRequest.responseText)
             console.log(response);
             // console.log(response.RevenueTTM)
-            result[0] = response.RevenueTTM;
-            document.getElementById(`ttm`).innerText = response.RevenueTTM;            
+            result[0] = response.RevenueTTM/ 1000000;
+            document.getElementById(`ttm`).innerText = result[0];
             document.getElementById("company-name").innerText = response.Name;
             document.getElementById("company-description").innerText =  response.Description;
             document.getElementById("exchange").innerText = response.Exchange;
@@ -55,18 +53,32 @@ function alphaVantageQuery(symbol)
     };
     
     ttmRequest.open("GET", ttmUrl, true);
-    ttmRequest.send();
+    ttmRequest.send()
+    
+    setTimeout(()=>financialsRequest.send(), 1500);
     
     return result;
 }
 
+function cleardata()
+{
+    document.getElementById(`revenue-chart`).src = `./chartjs/?0&0&0&0&0&0`
+    for (let i = 1; i < 6; i++) {                
+        document.getElementById(`m${i}`).innerText = "";
+    }
+    document.getElementById(`ttm`).innerText =  ""
+    document.getElementById("company-name").innerText =  ""
+    document.getElementById("company-description").innerText = ""
+    document.getElementById("exchange").innerText =  ""
+    document.getElementById("sector").innerText =    ""
+}
 // AJAX query
 // return result
 
 addEventListener('keydown', function (e) {
     if (e.key === 'Enter') {
-      symbol = document.getElementById(`searchbar`).value.toUpperCase().replaceAll(" ",'')
-      console.log(symbol)
-      alphaVantageQuery(symbol)
+        symbol = document.getElementById(`searchbar`).value.toUpperCase().replaceAll(" ",'')
+        console.log(symbol)
+        alphaVantageQuery(symbol)
     }
 });
